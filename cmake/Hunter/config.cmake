451e362facac03a6751e6d0e13404cf85d2f7765
Ibai Gorordo <43162939+ibaiGorordo@users.noreply.github.com>
2022-03-01 20:34:21 +0900

    Fix SHA1

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -3,7 +3,7 @@ hunter_config(
     libusb-luxonis
     VERSION "1.0.24-cmake"
     URL "https://github.com/ibaiGorordo/libusb/archive/2332baa9118579e93b254b131a5769c7b55c96af.tar.gz"
-    SHA1 "2332baa9118579e93b254b131a5769c7b55c96af"
+    SHA1 "db2c23460a9153d6f678bc769fea41972c7359d0"
     CMAKE_ARGS
         WITH_UDEV=OFF
 )
